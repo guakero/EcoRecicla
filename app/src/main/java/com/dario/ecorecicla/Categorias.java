66c2a8c4d6b5e88d5de28d7a9bb83ce20595674b
Cristian <casp33355@gmail.com>
2023-10-26 10:45:48 -0500

    se conectan 4 botones

--- app/src/main/java/com/dario/ecorecicla/Categorias.java
+++ app/src/main/java/com/dario/ecorecicla/Categorias.java
@@ -2,7 +2,11 @@ package com.dario.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Categorias extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class Categorias extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
+        ImageButton papel = findViewById(R.id.btnPapel);
+        ImageButton Home = findViewById(R.id.btnHome);
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Categorias.this, com.dario.ecorecicla.Home.class));
+            }
+        });
+        papel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Categorias.this, Categorias2.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/dario/ecorecicla/Categorias2.java
+++ app/src/main/java/com/dario/ecorecicla/Categorias2.java
@@ -5,23 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
-public class Home extends AppCompatActivity {
-
-
+public class Categorias2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Button registro = findViewById(R.id.btnRegistro);
-        registro.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_categorias2);
+        ImageButton regresar = findViewById(R.id.btnRegresar);
+        regresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Home.this, Categorias.class));
+                startActivity(new Intent(Categorias2.this, Categorias.class));
             }
         });
-
     }
 }
